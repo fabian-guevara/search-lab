ebe1ae9cea927f7e03fc20898c6f182eadefcb1e
fabian-guevara <fabiangviveros@gmail.com>
2024-08-15 17:41:20 -0600

    added concurrently

--- backend/searchRoutes.js
+++ backend/searchRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const client = require('./database.js'); // Import the productColl from a database file
+    const client = require('./database.js'); // Import the productColl from a database file
 
 const productColl =client.db("products").collection("products");
 
