f583d1677455d2a6350751e11474a08ec45217cf
fabian-guevara <fabiangviveros@gmail.com>
2024-08-15 17:40:39 -0600

    added seed conditionally

--- backend/seed.js
+++ backend/seed.js
@@ -1,18 +1,40 @@
-const { MongoClient } = require("mongodb");
-
-// Replace with your MongoDB connection string
-const uri = 'mongodb+srv://user:123@demo.lfvxb.mongodb.net/?retryWrites=true&w=majority&appName=DEMO';
+const client = require('./database.js');
 
 
-const client = new MongoClient(process.env.MONGODB_URI);
 
+const seedDatabase = async () => {
+    try {
+        // Connect to the MongoDB client
+        await client.connect();
 
-fetch('https://fakestoreapi.com/products/')
-            .then(res=>res.json())
-            .then(async json=>{
-                await client.connect()
-                    client.db("products").collection("products").insertMany(json)
-                    console.log("Data seeded");
-})
+        const db = client.db("ecommerce");
+        
+        // Fetch the collections in the database
+        const collections = await db.listCollections().toArray();
+        const collectionNames = collections.map(collection => collection.name);
+        
+        // Check if the "products" collection exists
+        if (!collectionNames.includes("products")) {
+            console.log("Seeding products collection...");
+            
+            // Fetch products from the Fake Store API
+            const response = await fetch('https://fakestoreapi.com/products/');
+            const jsonResponse = await response.json();
 
+            // Insert products into the database
+            await db.collection("products").insertMany(jsonResponse);
 
+            console.log("Database seeded successfully. Happy demoing!");
+        } else {
+            console.log("Products collection already exists. Skipping seeding.");
+        }
+    } catch (error) {
+        console.error("There was an error seeding the database", error.message);
+    } finally {
+        // Close the client connection
+        await client.close();
+    }
+};
+
+// Run the function
+seedDatabase();
